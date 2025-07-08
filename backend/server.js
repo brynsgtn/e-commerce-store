ba8400aa1713019e776b511cac45b760b3c01919
brynsgtn <bryansuguitan17@gmail.com>
2025-07-08 16:49:00 -0500

    Added payment model, routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "../backend/routes/auth.route.js";
 import productRoutes from "../backend/routes/product.route.js";
 import cartRoutes from "../backend/routes/cart.route.js";
 import couponRoutes from "../backend/routes/coupon.route.js";
+import paymentRoutes from "../backend/routes/payment.route.js";
 
 
 import { connectDB } from "./lib/db.js";
@@ -23,6 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
