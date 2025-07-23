63a7676522057047557c1bff5cc49a88fe653dd3
brynsgtn <bryansuguitan17@gmail.com>
2025-07-23 10:37:01 -0500

    Implement analytics route and controller

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from "../backend/routes/product.route.js";
 import cartRoutes from "../backend/routes/cart.route.js";
 import couponRoutes from "../backend/routes/coupon.route.js";
 import paymentRoutes from "../backend/routes/payment.route.js";
+import analyticsRoutes from "../backend/routes/analytics.route.js";
 
 
 import { connectDB } from "./lib/db.js";
@@ -25,6 +26,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
