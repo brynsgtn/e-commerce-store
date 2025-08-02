c42d50dda128f4975df9220c884feba27a68a273
brynsgtn <bryansuguitan17@gmail.com>
2025-08-02 14:46:00 -0500

    Add LoadingSpinner and implement

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -34,5 +34,14 @@ export const useUserStore = create((set, get) => ({
             toast.error(error.response.data.message || "An error occured");
         }
     },
-    
-}))
+    checkAuth: async () => {
+        set({ checkingAuth: true });
+        try {
+            const response = await axios.get("/auth/profile");
+            set({ user: response.data, checkingAuth: false });
+        } catch (error) {
+            console.log(error.message);
+            set({ checkingAuth: false, user: null });
+        }
+    },
+}));
