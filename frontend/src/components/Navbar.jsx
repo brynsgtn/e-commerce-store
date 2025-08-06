c460e28111dd7e8cd4dfa7ede710bd4c915d8091
brynsgtn <bryansuguitan17@gmail.com>
2025-08-06 14:52:18 -0500

    Implement cart item count

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const Navbar = () => {
     const { user, logout } = useUserStore();
     const isAdmin = user?.role === "admin";
-
+    const { cart } = useCartStore();
     return (
         <header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
             <div className='container mx-auto px-4 py-3'>
@@ -26,7 +27,7 @@ const Navbar = () => {
                                     className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out'
                                 >
-                                    3
+                                    {cart.length}
                                 </span>
                             </Link>
                         )}
@@ -44,7 +45,7 @@ const Navbar = () => {
                             <button
                                 className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
 						rounded-md flex items-center transition duration-300 ease-in-out'
-                            onClick={logout}
+                                onClick={logout}
                             >
                                 <LogOut size={18} />
                                 <span className='hidden sm:inline ml-2'>Log Out</span>
