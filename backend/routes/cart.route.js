b16adde612a9bef6fa1ba70bfe9e35d4d7c918d9
brynsgtn <bryansuguitan17@gmail.com>
2025-08-17 14:38:02 -0500

    Implement clearCart route

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { addToCart, removeAllFromCart, updateQuantity, getCartProducts } from "../controllers/cart.controller.js";
+import { addToCart, removeAllFromCart, updateQuantity, getCartProducts, clearCart } from "../controllers/cart.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getCartProducts);
 router.post("/", protectRoute, addToCart);
+router.post("/clear", protectRoute, clearCart);
 router.delete("/", protectRoute, removeAllFromCart);
 router.put("/:id", protectRoute, updateQuantity);
 
