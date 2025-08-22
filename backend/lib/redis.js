1d85a29b67737407f4eb93099dda003614c91b08
brynsgtn <bryansuguitan17@gmail.com>
2025-08-22 14:05:37 -0500

    Fix error

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -4,4 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
+  tls: {}, // force TLS
+  maxRetriesPerRequest: null, // recommended for serverless
+  enableReadyCheck: false,    // Upstash doesn’t support INFO command
+  retryStrategy(times) {
+    return Math.min(times * 50, 2000); // reconnect if dropped
+  },
+});
